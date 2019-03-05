b2e266c38211014ee6f93683ed202f6ae82d32a7
Slezák Michal <michal.slezak@qbsw.sk>
2019-03-05 10:09:25 +0100

    Initial commit

---
+++ wfm-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iihf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wfm-web';
+}
