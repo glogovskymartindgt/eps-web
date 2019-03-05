5cb8f52294ce759e1119953c420534b2f4a2c67f
Slezák Michal <michal.slezak@qbsw.sk>
2019-03-05 14:36:34 +0100

    IIHF-10 Web - multilanguage preparations

--- wfm-web/src/app/app.component.ts
+++ wfm-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'iihf-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wfm-web';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+  
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
+
 }
--- wfm-web/src/app/app.module.ts
+++ wfm-web/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// import ngx-translate and the http loader for translations
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     //ngx-translate imports
+     HttpClientModule,
+     TranslateModule.forRoot({
+         loader: {
+             provide: TranslateLoader,
+             useFactory: HttpLoaderFactory,
+             deps: [HttpClient]
+         }
+     })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// ngx-translate required HttpLoaderFactory for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
