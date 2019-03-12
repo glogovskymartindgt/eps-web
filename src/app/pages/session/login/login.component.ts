3162436c9af4f089f98ad9c9fad98c06f817cfd8
Slezák Michal <michal.slezak@qbsw.sk>
2019-03-12 13:11:42 +0100

    IIHF-26 - Login Screen updated

--- src/app/pages/session/login/login.component.ts
+++ src/app/pages/session/login/login.component.ts
@@ -2,7 +2,9 @@ import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-// import { TRANSLATE } from 'src/app/shared/custom-functions';
+import { TRANSLATE } from 'src/app/shared/custom-functions';
+
+import {_} from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
 
 @Component({
   selector: 'iihf-login',
@@ -11,46 +13,35 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  public hide  = true;
+  public hidePassword  = true;
 
   public loginForm: FormGroup;
 
-  // public sth = TRANSLATE('login.login');
-
-  public constructor(private readonly router: Router, private fb: FormBuilder, private translate: TranslateService) { }
+  public constructor(private readonly router: Router,
+                     private readonly formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  
+
     this.createForm();
 
   }
 
   private createForm() {
-    this.loginForm = this.fb.group({
-      email: [null, [Validators.required, Validators.email]],
+
+    this.loginForm = this.formBuilder.group({
+      loginName: [null, [Validators.required]],
       password: [null, [Validators.required]]
     });
+
   }
 
-  get f() { return this.loginForm.controls; }
-
-  getErrorMessage(value: 'email' | 'password' ) {
-    if (value === 'email') {
-      return this.f.email.hasError('required') ? 'You must enter a value' :
-        this.f.email.hasError('email') ? 'Not a valid email' :'';
-    } else if (value === 'password') {
-      return 'Password is required';
-      // return TRANSLATE('login.login');
-    }
-  }
+  get loginFormControls() { return this.loginForm.controls; }
 
   public login() {
 
     if (this.loginForm.invalid) {
       return;
     }
-
-    // alert("Form values" + JSON.stringify(this.loginForm.value));
 
     this.router.navigate(['/tasks']);
 
