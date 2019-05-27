de4ce4b30f45f463494c0d9b8b16e8c9b3214924
Slezák Michal <michal.slezak@qbsw.sk>
2019-05-27 08:57:35 +0200

    version increased 1.0.0

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
     // Current project version, also need to be changed in package.json file
-    public static readonly version = '0.1.3-M5';
+    public static readonly version = '1.0.0';
 }
