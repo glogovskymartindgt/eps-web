5c6f6e6a3f62620fedcba7425fdf9ba608dd3178
gregor.albert <gregor.albert@qbsw.sk>
2019-09-05 12:36:11 +0200

    IIHF-277 Rename fields and version update

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
     // Current project version, also need to be changed in package.json file
-    public static readonly version = '1.1.0';
+    public static readonly version = '1.2.0';
 }
