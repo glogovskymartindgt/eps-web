b51d60b3e46909a02f8d2db475f84484ed0cf11b
gregor.albert <gregor.albert@qbsw.sk>
2019-10-15 12:45:42 +0200

    Version 1.3.0

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
     // Current project version, also need to be changed in package.json file
-    public static readonly version = '1.2.2';
+    public static readonly version = '1.3.0';
 }
