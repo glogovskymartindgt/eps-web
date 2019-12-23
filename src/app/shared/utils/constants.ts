84a3d2659263576eabb87f0df3de3bf3bd249df6
gregor.albert <gregor.albert@qbsw.sk>
2019-12-23 10:25:39 +0100

    IIHF-490

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-    public static readonly version = '1.6.1';
+    public static readonly version = '1.6.2';
     public static readonly defaultAvatarPath = 'assets/img/avatar.svg';
 }
