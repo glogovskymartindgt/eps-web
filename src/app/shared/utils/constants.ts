4f6a7249a04ad3a5af473e1b9487bb4da92a707c
gregor.albert <gregor.albert@qbsw.sk>
2020-02-13 11:53:04 +0100

    version 1.7.0

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-    public static readonly version = '1.6.2';
+    public static readonly version = '1.7.0';
     public static readonly defaultAvatarPath = 'assets/img/avatar.svg';
 }
