aa18fbb6a4bcf6350a7769642e70aea311596c23
gregor.albert <gregor.albert@qbsw.sk>
2020-03-31 08:41:50 +0200

    IIHF-556 Comments (in task detail): no text displayed in comments with type TEXT

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-    public static readonly version = '1.8.0';
+    public static readonly version = '1.8.1';
     public static readonly defaultAvatarPath = 'assets/img/avatar.svg';
 }
