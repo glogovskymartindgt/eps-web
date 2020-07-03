63bcadd59dc1b24f40961fb54bc6db29402dd76a
daniel.bozik <daniel.bozik@gbsw.sk>
2020-07-03 11:18:06 +0200

    IIHF-628 - Action points (overview): screen is not correctly shown after editing of record when user sorted data according to "Meeting text"

--- src/app/pages/tasks/tasks.module.ts
+++ src/app/pages/tasks/tasks.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { TableChangeStorageService } from '../../shared/services/table-change-storage.service';
 import { TranslateWrapperService } from '../../shared/services/translate-wrapper.service';
 import { SharedModule } from '../../shared/shared.module';
 import { TaskCreateComponent } from './task-create/task-create.component';
 import { TaskEditComponent } from './task-edit/task-edit.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskOverviewPersistanceService } from './task-overview-persistance.service';
 import { TasksRoutingModule } from './tasks-routing.module';
 
 @NgModule({
@@ -13,7 +15,11 @@ import { TasksRoutingModule } from './tasks-routing.module';
         SharedModule,
         TasksRoutingModule,
     ],
-    providers: [TranslateWrapperService]
+    providers: [
+        TranslateWrapperService,
+        TableChangeStorageService,
+        TaskOverviewPersistanceService,
+    ]
 })
 export class TasksModule {
 }
