222f142b19a7c9cdab272c6a912f9baa45d0b39b
daniel.bozik <daniel.bozik@gbsw.sk>
2020-07-06 09:53:24 +0200

    version back to 1.9.0

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
-    public static readonly version = '1.9.1';
+    public static readonly version = '1.9.0';
     public static readonly defaultAvatarPath = 'assets/img/avatar.svg';
     public static readonly defaultTablePageSize = 100;
 }
