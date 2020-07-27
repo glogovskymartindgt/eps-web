f47e815f8f0e7f99a7b22f810bb3cfdaa8d3114f
daniel.bozik <daniel.bozik@gbsw.sk>
2020-07-27 16:21:14 +0200

    IIHF-680 - avatar component

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,12 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-import { AbstractInputsModule } from '../../shared/hazelnut/abstract-inputs';
-import { MaterialModule, SharedDirectivesModule } from '../../shared/hazelnut/hazelnut-common';
-import { PipesModule } from '../../shared/pipes/pipes.module';
+import { SharedDirectivesModule } from '../../shared/hazelnut/hazelnut-common';
+import { SharedModule } from '../../shared/shared.module';
 import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 
 import { ProfileRoutingModule } from './profile-routing.module';
@@ -14,15 +9,10 @@ import { ProfileRoutingModule } from './profile-routing.module';
 @NgModule({
     declarations: [ProfileDetailComponent],
     imports: [
-        CommonModule,
         ProfileRoutingModule,
-        MaterialModule,
-        FlexLayoutModule,
         TranslateModule.forChild(),
-        ReactiveFormsModule,
-        AbstractInputsModule,
-        PipesModule,
-        SharedDirectivesModule
+        SharedDirectivesModule,
+        SharedModule,
     ]
 })
 export class ProfileModule {
--- src/app/shared/services/core/file.service.ts
+++ src/app/shared/services/core/file.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -14,5 +15,22 @@ export class FileService {
             func(reader.result as string);
         };
         reader.readAsDataURL(blob);
+    }
+
+    public readFile$(file: File | Blob): Observable<string> {
+        const result: Subject<string> = new Subject<string>();
+
+        const reader = new FileReader();
+        reader.onload = (): void => {
+            result.next(reader.result as string);
+            result.complete();
+        };
+        reader.onerror = (error: any): void => {
+            result.error(error);
+            result.complete();
+        };
+        reader.readAsDataURL(file);
+
+        return result.asObservable();
     }
 }
