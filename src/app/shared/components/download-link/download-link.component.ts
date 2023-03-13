61e9af71a04418c3a250fb83984a219149228512
tomas.trusik <trusik@dgtfactory.com>
2023-03-13 10:50:37 +0100

    IIEPS-138: Added button to copy URL to clipboard

--- src/app/shared/components/download-link/download-link.component.ts
+++ src/app/shared/components/download-link/download-link.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NotificationService } from '../../services/notification.service';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
     selector: 'iihf-download-link',
@@ -11,14 +13,20 @@ export class DownloadLinkComponent {
     @Input() public src: string;
     @Input() public fileName: string;
     @Input() public fileTitle: string;
+    @Input() public copyUrlButton: boolean;
 
     public constructor(
         private readonly domSanitizer: DomSanitizer,
+        protected readonly notificationService: NotificationService,
+        private clipboard: Clipboard
     ) {
     }
 
     public sanitize(src: string): SafeResourceUrl {
         return this.domSanitizer.bypassSecurityTrustResourceUrl(src);
     }
-
+    public copyUrl() {
+        this.clipboard.copy(window.location.href);
+        this.notificationService.openSuccessNotification('success.linkCopied')
+    }
 }
