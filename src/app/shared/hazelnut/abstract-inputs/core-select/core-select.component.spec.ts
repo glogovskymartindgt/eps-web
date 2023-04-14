cbeb06460b71052f9f7a2ff05f12d4972aa7f02b
LENKOVA11\janka <lenkova@dgtfactory.com>
2023-04-14 09:54:09 +0200

    IIEPS-179 add select component for yes/no options

---
+++ src/app/shared/hazelnut/abstract-inputs/core-select/core-select.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoreSelectComponent } from './core-select.component';
+
+describe('CoreSelectComponent', () => {
+  let component: CoreSelectComponent;
+  let fixture: ComponentFixture<CoreSelectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CoreSelectComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CoreSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
