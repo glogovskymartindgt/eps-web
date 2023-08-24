dc18d500fb535b1e0c08a63bc2207e08e099507a
Martin Glogovský <glogysv@gmail.com>
2023-08-24 11:29:20 +0200

    Update of the app to version 1.14.1

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
-    public static readonly version = '1.14.0';
+    public static readonly version = '1.14.1';
     public static readonly defaultAvatarPath = 'assets/img/avatar.svg';
     public static readonly defaultTablePageSize = 100;
 }
